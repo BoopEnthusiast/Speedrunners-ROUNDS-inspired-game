db41e0dcdc1ddaf956574b2a5be518587717715e
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-03-10 19:25:10 +1300

    Update to 4.4 and add more debugging information

--- globals/networking.gd
+++ globals/networking.gd
@@ -6,20 +6,25 @@ var steam_username: String = ""
 
 
 func _init() -> void:
-	OS.set_environment("SteamAppID", str(480))
-	OS.set_environment("SteamGameID", str(480))
+	print("Log file is in: ")
+	print(OS.get_user_data_dir())
+	
+	print("Setting Steam app/game environment variables...")
+	OS.set_environment("SteamAppID", str(3540120))
+	OS.set_environment("SteamGameID", str(3540120))
 
 
 func _ready() -> void:
 	var steam_init_response: Dictionary = Steam.steamInitEx()
-	print("Steam init: ", steam_init_response)
+	print("Steam init response: ", steam_init_response)
 	
-	if steam_init_response['status'] > 0:
+	if steam_init_response['status'] > Steam.STEAM_API_INIT_RESULT_OK:
 		print("Failed to initialize Steam, shutting down: ", steam_init_response)
-		get_tree().quit()
 	
 	steam_id = Steam.getSteamID()
 	steam_username = Steam.getPersonaName()
+	print("Steam ID: ", steam_id)
+	print("Steam Username: ", steam_username)
 
 
 func _process(_delta: float) -> void:
